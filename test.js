ea17bc92b8b107942aba4a127eb093c2e1dedd1c
Guillaume Martigny <guillaume.martigny@wiztivi.com>
2018-07-26 16:25:15 +0200

    Update or remove outdated dependencies Use AVA for test

--- test.js
+++ test.js
@@ -1,7 +1,7 @@
-/* global test expect */
-
+import test from "ava";
 import textDirection from "./text-direction";
 
-test("Basic usage", () => {
-    expect(textDirection()).toBe("");
+test("text-direction", (t) => {
+    t.is(textDirection.constructor.name, "Function");
+    t.throws(() => textDirection(), ReferenceError, "Don't work in a node environment.");
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,7 @@
-const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
-
 module.exports = {
     entry: "./text-direction.js",
-    plugins: [
-        new UglifyJsPlugin(),
-    ],
-    module: {
-        rules: [{
-            test: /\.js$/,
-            use: {
-                loader: "babel-loader",
-                options: {
-                    presets: ["@babel/preset-env"],
-                },
-            },
-        }],
-    },
     output: {
-        filename: "dist/text-direction.min.js",
+        filename: "text-direction.min.js",
         library: "textDirection",
         libraryTarget: "this",
         libraryExport: "default",
