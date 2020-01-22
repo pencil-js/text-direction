01231ff020ebc3d9b52887d82cd1b2aad45f883f
Guillaume Martigny <guillaume.martigny@gmail.com>
2020-01-22 15:26:41 +0100

    Update outdated dependencies

--- test.js
+++ test.js
@@ -1,8 +1,5 @@
 import test from "ava";
-import browserEnv from "browser-env";
 import textDirection from ".";
-
-browserEnv(["document"]);
 
 test("Is not crashing", (t) => {
     t.is(textDirection(), "");
