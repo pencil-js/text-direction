5ba06200d1fe4ae46bfc504269df90646e1bd13b
Guillaume Martigny <guillaume.martigny@gmail.com>
2023-09-12 16:40:34 +0200

    Remove webpack build

--- webpack.config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    entry: "./text-direction.js",
-    output: {
-        filename: "text-direction.min.js",
-        library: "textDirection",
-        libraryTarget: "this",
-        libraryExport: "default",
-    },
-};
